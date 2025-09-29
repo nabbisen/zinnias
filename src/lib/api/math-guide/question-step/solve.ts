2bd820038fe4e91ead3c6f5e4d4c499059f79cee
nabbisen <nabbisen@scqr.net>
2025-09-30 00:21:03 +0900

    optimize question step prompts

--- src/lib/api/math-guide/question-step/solve.ts
+++ src/lib/api/math-guide/question-step/solve.ts
@@ -1,10 +1,8 @@
-import { MATH_PROMPT_RESPONSE_FORMAT, MATH_PROMPT_START } from "$lib/constants/api/math-guide"
+import { MATH_PROMPT_RESPONSE_FORMAT, MATH_PROMPT_START, SOLVE_RESPONSE_TOKEN_MAX_NUMBER } from "$lib/constants/api/math-guide"
 import type { GenerateTone } from "$lib/types/common/prompt"
 import { generateTonePrompt } from "$lib/utils/api/common/generate"
 import { responseTokenMaxNumberPrompt } from "$lib/utils/api/math-guide/question-step"
 import type { Part } from "@google-cloud/vertexai"
-
-const RESPONSE_TOKEN_MAX_NUMBER = 700
 
 export function solvePrompt(question: string, generateTone: GenerateTone): Part[] {
     const prompt: Part[] = [
@@ -13,7 +11,7 @@ export function solvePrompt(question: string, generateTone: GenerateTone): Part[
         { text: `【回答内容】解答を論理展開を示しながら書く。論理展開は、初学者がわかる、易しいものにする。解法は初学者向きのものにして、高度な公式や複雑な定理は使わない。定理を使う場合は、該当する条件や紐づく対象を正確に書く (例えば三角形の相似条件を使う時は「２組の辺の比とその間の角がそれぞれ等しいから」のような条件を明記する)。` },
         { text: '【回答内容に関する補足: 解法の選択】初学者向けの標準的な解法が複数ある場合、最も人気の高いものを選ぶ。 回答文字数に余裕がある場合に限って、もしも他に人気が近くてかつ標準的な解法があれば、それに関するヒントを添えても良い。' },
         { text: `【回答書式】${MATH_PROMPT_RESPONSE_FORMAT}` },
-        { text: `【回答条件】解答の最終形と関わりが無いものは、回答に書かない (例えば、あなたの試行錯誤の途中経過など)。 ${responseTokenMaxNumberPrompt(RESPONSE_TOKEN_MAX_NUMBER)} 回答にあなた自身のメッセージは書かない。` },
+        { text: `【回答条件】解答の最終形と関わりが無いものは、回答に書かない (例えば、あなたの試行錯誤の途中経過など)。 ${responseTokenMaxNumberPrompt(SOLVE_RESPONSE_TOKEN_MAX_NUMBER)} 回答にあなた自身のメッセージは書かない。` },
         generateTonePrompt(generateTone),
     ]
     return prompt
--- src/lib/constants/api/math-guide.ts
+++ src/lib/constants/api/math-guide.ts
@@ -8,3 +8,7 @@ export const PROMPT_START_WITH_IMAGE = 'あなたは学習教材のページ画�
 
 export const MATH_PROMPT_START = 'あなたは数学の学習教材の内容解説を子供に向けて行うプロフェッショナルです。次のタスクを行いなさい。'
 export const MATH_PROMPT_RESPONSE_FORMAT = '【回答書式】Markdown 形式の文字列。テキスト言語は日本語。 数式および変数は KaTeX 記法で記述する。数式は $$ 囲みでディスプレイ表記にする。LaTeX 記法および MathML 記法は使わない。'
+
+export const DESCRIBE_RESPONSE_TOKEN_MAX_NUMBER = 200
+export const EXPLAIN_RESPONSE_TOKEN_MAX_NUMBER = 300
+export const SOLVE_RESPONSE_TOKEN_MAX_NUMBER = 300
